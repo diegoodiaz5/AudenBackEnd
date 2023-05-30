e975b6ef3e0e248c5df57d34f161a61351c9e1e1
Diego Díaz <diego_diaz9990@hotmail.com>
2023-05-30 04:35:52 -0300

    Controlador de iniciar Sesion Creado y terminado (verifica por nombre/email)

---
+++ controllers/iniciarSesion.js
@@ -0,0 +1,45 @@
+const { Pool } = require("pg");
+const client = new Pool({
+    user: "postgres",
+    host: "localhost",
+    database: "audn",
+    password: "apka",
+    port: 3001
+});
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const { TOKEN_SECRET } = require("../middlewares/validar-jwt");
+
+exports.iniciarSesion = async (req, res) => {
+    const { info, contraseña } = req.body;
+
+    try {
+        const { rows } = await client.query(
+            "SELECT * FROM users WHERE email = $1 OR nombre_de_usuario = $1",
+            [info]
+        );
+        if (rows[0] !== undefined) {
+            const validPassword = await bcrypt.compare(
+                contraseña,
+                rows[0].contraseña
+            );
+            if (!validPassword) {
+                return res.status(400).json({ error: "Contraseña no válida" });
+            } else {
+                const token = jwt.sign(
+                    {
+                        name: rows[0].nombre_de_usuario,
+                        id: rows[0].user_id,
+                    },
+                    TOKEN_SECRET
+                );
+                res.json({ error: null, data: "Login exitoso", token });
+            }
+        } else {
+            return res.send({ error: "Usuario no encontrado" });
+        }
+    } catch (error) {
+        res.send(error)
+    }
+
+}
