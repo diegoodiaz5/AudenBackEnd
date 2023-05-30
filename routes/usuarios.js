a80292a2ab12e06e687fdd1e243c07b88908ba49
Diego Díaz <diego_diaz9990@hotmail.com>
2023-05-30 04:36:03 -0300

    Ruta de iniciar Sesion creada

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { nuevoUsuario, verificarEmail, verificarNombreUsuario } = require("../controllers/nuevoUsuario");
 const { usuarios } = require("../controllers/listaUsuarios");
+const { iniciarSesion } = require("../controllers/iniciarSesion")
 
 // Lista de usuarios registrados
 router.get("/", usuarios);
@@ -14,5 +15,8 @@ router.post("/verificarEmail", verificarEmail);
 
 // Verificar si el nombr de usuario ya existe
 router.post("/verificarNombreUsuario", verificarNombreUsuario)
+
+// Login de un usuario
+router.post("/iniciarSesion", iniciarSesion);
 
 module.exports = router;
