c473005e4b5323b2bf8714faf893d63fa7d96db6
Diego Díaz <diego_diaz9990@hotmail.com>
2023-05-31 22:51:10 -0300

    Creada ruta para el controlador "verificarUsuario"

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { nuevoUsuario, verificarEmail, verificarNombreUsuario } = require("../controllers/nuevoUsuario");
+const { nuevoUsuario, verificarEmail, verificarNombreUsuario, verificarUsuario } = require("../controllers/nuevoUsuario");
 const { usuarios } = require("../controllers/listaUsuarios");
 const { iniciarSesion } = require("../controllers/iniciarSesion")
 
@@ -13,8 +13,11 @@ router.post("/nuevousuario", nuevoUsuario);
 // Verificar si el email está registrado
 router.post("/verificarEmail", verificarEmail);
 
-// Verificar si el nombr de usuario ya existe
-router.post("/verificarNombreUsuario", verificarNombreUsuario)
+// Verificar si el nombre de usuario ya existe
+router.post("/verificarNombreUsuario", verificarNombreUsuario);
+
+// Verificar si el usuario existe por email/nombre de usuario
+router.post("/verificarUsuario", verificarUsuario);
 
 // Login de un usuario
 router.post("/iniciarSesion", iniciarSesion);
